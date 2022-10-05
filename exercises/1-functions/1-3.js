8ac6080c35cbe3cb4fa7cec1ef8053050e308b0d
Anke Van Humbeeck <anke.van.humbeeck@student.ehb.be>
2022-10-05 16:07:51 +0200

    exercise 1.3

---
+++ exercises/1-functions/1-3.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d"); 
+
+drawRectangles();
+
+function drawRectangles() {
+    context.strokeStyle = "red";    
+    context.beginPath();
+    context.moveTo(50,50);
+    context.rect(50,50,150,150);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(200,200);
+    context.rect(200,200,150,150);
+    context.rect(275,75,50,50);
+    context.rect(75,275,50,50);
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = "black";
+    context.rect(125,125,150,150);
+    context.rect(325,50,25,25);
+    context.rect(50,325,25,25);
+    context.fill();
+    context.stroke();
+
+}
