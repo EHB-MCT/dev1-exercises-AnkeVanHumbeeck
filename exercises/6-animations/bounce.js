98342b8392b1f082d76d97f46fb7d4702883ecf2
Anke Van Humbeeck <anke.van.humbeeck@student.ehb.be>
2022-11-18 14:29:09 +0100

    working stop

--- exercises/6-animations/bounce.js
+++ exercises/6-animations/bounce.js
@@ -6,12 +6,6 @@ import * as Utils from "../../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-/*Draw a circle which moves diagonally on the screen.
-Use 2 global variables to store the x and y position of the circle.
-The circle moves 10 pixels horizontally and 10 pixles vertically each frame.
-When the cirlcle touches the sides of the window the horizontal movement is reversed. When it touches the bottom or top of the window the vertical movement is reversed. */
-
-let frameCount = 0;
 let ismovingRight = true;
 let ismovingUp = true;
 let xPos = 50;
@@ -21,18 +15,16 @@ var speedX = 5;
 var speedY = 5;
 
 update();
+window.onclick = click;
 
-function update(currentTime) {
-	let lastFrameTime = 0;
-
+function update() {
+	/*if (click) {
+		context.fillStyle = "white";
+		context.fillRect(0, 0, width, height);
+	} else {
+	}*/
 	context.fillStyle = "white";
 	context.fillRect(0, 0, width, height);
-	frameCount++;
-	lastFrameTime = currentTime;
-
-	/*if (frameCount % 10 == 0) {
-		console.log(frameCount);
-	}*/
 
 	context.fillStyle = "blue";
 	Utils.fillCircle(xPos, yPos, 50);
@@ -60,21 +52,22 @@ function update(currentTime) {
 	}
 }
 
-window.onclick = click;
-let distance = Utils.calculateDistance(xPos, yPos, xPos + 50, yPos + 50);
+/**
+ *
+ * @param {MouseEvent} eventData
+ */
 
 function click(eventData) {
 	let x = eventData.pageX;
 	let y = eventData.pageY;
-	//console.log(click);
+	let distance = Utils.calculateDistance(xPos, yPos, x, y);
 
-	if (window.onclick) {
-		console.log("I clicked");
+	if (distance < 50) {
 		//sets speed to 0
 		speedX = 0;
 		speedY = 0;
-		//draws red circle
+		//draws red circle, because of the clear canvas, not drawn
 		context.fillStyle = "red";
-		Utils.fillCircle(xPos, yPos, 150);
+		Utils.fillCircle(x, y, 50 / 3);
 	}
 }
