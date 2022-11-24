11bfb20e4e5f77e77df08b5ec63216ae05141d9a
Anke Van Humbeeck <anke.van.humbeeck@student.ehb.be>
2022-11-24 16:57:03 +0100

    demo

---
+++ exercises/demos/demonaamgen.js
@@ -0,0 +1,16 @@
+"use strict";
+
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+context.fillStyle = "black";
+context.fillRect(0, 0, width, height);
+
+let names = ["Peter", "Ali", "Sofia", "Bent", "Laura"];
+
+let r = Utils.randomNumber(0, names.length - 1);
+
+for (let i = names.length - 1; i < 5; i++) console.log(names[i]);
